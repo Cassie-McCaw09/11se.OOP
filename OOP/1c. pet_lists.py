6493566a050089ed2b8c210bb29414206e93173d
Cassie-McCaw09 <mccawcassie983@gmail.com>
2026-05-12 04:06:50 +0000

    completed

--- OOP/1c. pet_lists.py
+++ OOP/1c. pet_lists.py
@@ -1,26 +1,22 @@
 #Tutorial 3 Lists:
 #1. Create an example of parallel lists eg: pet_name, species, age, vaccination_status for three pets
-names = ['Rocky', 'Foxy', 'Bella']
-animal_categories = ['Cat', 'Fox', 'Rabbit']
-ages = [5,8,3]
+names = ['Foxy', 'Bella', 'Hootie']
+animal_categories = [ 'Dog', 'Rabbit', 'Blowfish']
+ages = [8,3,34]
+vaccinated = [ False, False, True]
 
-for name in names:
-    print(name)
+for i in range(len(names)):
+    print('Pet_name:', names[i])
+    print('Category:', animal_categories[i])
+    print('Age:', ages[i])
+    print('Vaccinated:',vaccinated[i])
+    print('')
+
 #2. Use a for loop to print parallel list details. This will mean that one complete printout will look like:
-'''
-Pet name: Foxy
-Species: Dog
-Age: 8
-Vaccination Status: False
-'''
 #3. Demonstrate what happens when an item is deleted
-
-
-
 
   #ACTIVITIES:
 # In each activity test out that the printing of data is still valid
-
 #1. Add a new animal named Hootie, its a blowfish, it is 34 years
 #2. Vaccinate an unvaccinated animal (create vaccination)
 #3. Remove an animal and make sure that all the printing is correct
