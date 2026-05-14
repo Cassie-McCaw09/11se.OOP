1966056f07e18ba0f1d7478c055990dbb490cba7
Cassie-McCaw09 <mccawcassie983@gmail.com>
2026-05-14 09:53:38 +0000

    completed

--- OOP/4a. OOP_methods_activities.py
+++ OOP/4a. OOP_methods_activities.py
@@ -13,6 +13,40 @@ class Pet:
     
     def have_birthday(self):
         self.age += 1
+
+    def vaccinate(self):
+        if self.vaccinated == True:
+            print(self.name, 'is already vaccinated')
+        else:
+            self.vaccinated = True
+    
+    def clear_balance(self):
+        self.account_balance = 0
+
+        def calculate_human_age(self):
+            if self.category == 'Dog':
+                print(self.name,'human age:',self.age*7)
+            elif self.category == 'Cat':
+                print(self.name,'human age:',self.age*6)
+            else:
+                print(self.name,'human age is unknown')
+
+        def __str__(self):
+             payment_status = 'unregistered'
+        if len(self.ccard) == 19:
+            payment_status = 'registered'
+
+        my_status = 'Name: ' + self.name +'\nCategory: ' + self.category + '\nAge: ' + str(self.age) +'\nPayment status: ' + payment_status + '\nVaccinated: '+ str(self.vaccinated)
+        return my_status
+
+p1 = Pet(name = 'Bonnie', category = 'Cat', age = 10)
+p1.have_birthday()
+p1.vaccinate()
+p1.vaccinate()
+
+print(p1)
+p1.calculate_human_age()
+
     
     
 
@@ -20,4 +54,4 @@ class Pet:
 #1. Add another method to vaccinate the pet
 #2. Add another attribute for account balance then add a method to clear balance
 #3. Add a method to print the animals age in human years use a multiplier of 7 if animal is a dog and a multiplier of 6 if it is a cat
-# Use print statements to ensure you have comeplted each activity correctly.
+# Use print statements to ensure you have comeplted each activity correctly
