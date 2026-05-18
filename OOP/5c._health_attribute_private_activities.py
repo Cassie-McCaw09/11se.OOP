47e0a9200a31052222793ed7904bb7830b66f2ac
Cassie-McCaw09 <mccawcassie983@gmail.com>
2026-05-18 01:32:23 +0000

    completed

--- OOP/5c._health_attribute_private_activities.py
+++ OOP/5c._health_attribute_private_activities.py
@@ -7,14 +7,18 @@ import random, time
 class Fighter:
     def __init__(self,name, starting_health, weapon, shield):
         self.name = name
-        self.health = starting_health
+        self.__health = starting_health
         self.weapon = weapon
         self.shield = shield
   
     def report(self):
-        print(self.name+':'+ ' Health: '+ str(self.health))
+        print(self.name+':'+ ' Health: '+ str(self.__health))
 
-
+    def is_dead(self):
+        if self.__health <= 0:
+            return True 
+        else:
+            return False
 
     def random_attack(self):
         attack_power = random.randint(self.weapon/2, self.weapon*2)
@@ -32,3 +36,25 @@ class Fighter:
 
 you = Fighter('You',100,60,20)
 troll = Fighter('Troll',200,30,10)
+
+you.report()
+troll.report()
+
+while True:
+    print('You attack the troll')
+    troll.defend (you.random_attack())
+    troll.report()
+    time.sleep(3)
+    print('')
+    if troll.heatlh <= 0:
+        print('You win')
+        break
+    print('The troll attack you . . .')
+    you.defend (troll.random_attack())
+    you.report()
+    time.sleep(3)
+    if you.is_dead():
+        if you.health <= 0:
+            print ('The troll wins')
+        break
+    print('')
